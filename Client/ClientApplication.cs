605dff6b9b79d6c3e8dfad1cbc164cd252dcb354
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-12-11 11:25:22 +0500

    Домашняя работа номер 1 выполнена.

---
+++ Client/ClientApplication.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Server;
+
+namespace Client;
+
+public class ClientApplication
+{
+    /// <summary>
+    /// Статический метод, для отправление сообщение
+    /// </summary>
+    /// <param name="from">отправитель сообщение</param>
+    /// <param name="IP">IP адрес сервера</param>
+    private static void SendMessage(string from, string IP)
+    {
+        UdpClient udpClient = new UdpClient();
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(IP), 12345);
+
+        string messageText = string.Empty;
+            do
+            {
+                Console.Clear();
+                Console.Write("Введите сообщение:");
+                messageText = Console.ReadLine()!;
+            } while (string.IsNullOrEmpty(messageText));
+
+
+      var message = new Message() { Text =messageText, NicknameFrom = from, DateTime = DateTime.Now, NicknameTo = "Server" };
+      var json =  message.SerializeMessageToJson();
+
+        message.PrintInformation("Сервер отправил сообщение");
+        byte[] buffer = Encoding.UTF8.GetBytes(json);
+        udpClient.Send(buffer, buffer.Length, iPEndPoint);
+    }
+
+    /// <summary>
+    /// Статический метод, для ввода через консоль строку и передает две строку на другой статический метод SendMessage()
+    /// </summary>
+    public static void ConsoleReadlineToString()
+    {
+        Console.Write("Введите имя отправителя:");
+        string? nicknameFrom = Console.ReadLine();
+        Console.Write("Введите IP адрес:");
+        string? ipAddress = Console.ReadLine();
+
+        SendMessage(nicknameFrom!, ipAddress!);
+    }
+}
---
+++ NetworkAppCSharp/ServerApplication.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+public class ServerApplication
+{
+    /// <summary>
+    /// Статический метод, для работы с сервером. Сервер получает информацию и выводет на консоль
+    /// </summary>
+    public static void Server()
+    {
+        UdpClient udpClient = new UdpClient(12345);
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        Console.WriteLine("Сервер ждёт сообщение от клиента");
+
+        while(true)
+        {
+
+
+            byte[] buffer = udpClient.Receive(ref iPEndPoint);
+            if(buffer == null) break;
+
+            var messageText = Encoding.UTF8.GetString(buffer);
+
+            Message? message = Message.DesearializeMessageFromJson(messageText);
+
+            message?.PrintInformation("Клиент отправил сообщение");
+        }
+    }
+}
